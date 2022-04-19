b902d373a7f7422ecb98855eb60ed11adc466a8e
Omar Elsayed <omar.alsayed@syscodeia.ae>
2022-04-19 02:16:14 +0200

    [ADMIN] [PROVIDER] [CLIENT] 1- Use Hero Library From Npm node modules

--- projects/admin/src/app/app.module.ts
+++ projects/admin/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HeroModule } from 'hero';
+import { HeroModule } from '@ngom/hero';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
